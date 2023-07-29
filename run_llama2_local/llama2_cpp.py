f02726f27dc8b0a1dfac5531a7d2c4f6f47ae591
cooperbaerseth <cooperbaer.seth@gmail.com>
2023-07-29 12:41:59 -0500

    attempting llama.cpp runs.. not working yet

---
+++ run_llama2_local/llama2_cpp.py
@@ -0,0 +1,24 @@
+"""
+using llama-cpp-python
+"""
+
+# python3 -m llama_cpp.server --model /Users/sethcooper-baer/Desktop/Desktop/personal_projects/llama2/llama_cpp/open-llama-7B-open-instruct.ggmlv3.q4_0.bin --n_gpu_layers 1
+
+# from llama_cpp import Llama
+
+# print("hey")
+
+# llm = Llama(model_path="/Users/sethcooper-baer/Desktop/Desktop/personal_projects/llama2/llama_cpp/llama-2-13b-chat.ggmlv3.q4_0.bin")
+
+# print(llm)
+
+
+"""
+using ctransformers
+"""
+
+from ctransformers import AutoModelForCausalLM
+
+llama2_path = "/Users/sethcooper-baer/Desktop/Desktop/personal_projects/llama2/llama_cpp/llama-2-13b-chat.ggmlv3.q4_0.bin"
+
+llm = AutoModelForCausalLM.from_pretrained(llama2_path, model_type='llama')
