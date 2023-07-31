562884614592d5956ecd3d6b8877cc54f653efba
cooperbaerseth <cooperbaer.seth@gmail.com>
2023-07-31 12:15:26 -0500

    small cleanup before PR

--- run_llama2_local/llama2_cpp.py
+++ run_llama2_local/llama2_cpp.py
@@ -1,19 +1,4 @@
 """
-using llama-cpp-python
-"""
-
-# python3 -m llama_cpp.server --model /Users/sethcooper-baer/Desktop/Desktop/personal_projects/llama2/llama_cpp/open-llama-7B-open-instruct.ggmlv3.q4_0.bin --n_gpu_layers 1
-
-# from llama_cpp import Llama
-
-# print("hey")
-
-# llm = Llama(model_path="/Users/sethcooper-baer/Desktop/Desktop/personal_projects/llama2/llama_cpp/llama-2-13b-chat.ggmlv3.q4_0.bin")
-
-# print(llm)
-
-
-"""
 using ctransformers
 """
 
